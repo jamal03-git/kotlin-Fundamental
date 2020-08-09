9e167a89e7b7a6062a6328baa73d6ffb9a939abd
jamal03-git <69096774+jamal03-git@users.noreply.github.com>
2020-08-09 22:30:46 +0700

    Add files via upload

---
+++ KotlinFundamental/src/main/kotlin/Data_types.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val firstword = "SMK ";
+    val middleword = "Telkom ";
+    val lastword = "Purwokerto";
+
+    val lebar: Int = 2;
+    val panjang: Int = 43;
+    val Luas: Int = lebar*panjang;
+
+    println(firstword + middleword + lastword);
+    println("Luas Persegi Panjang "+ Luas);
+}
---
+++ KotlinFundamental/src/main/kotlin/Hello_kotlin.kt
@@ -0,0 +1,8 @@
+fun main () {
+    val name ="jamal"
+    val hobby = "berenang dan bernyanyi"
+    print ("Nama saya ")
+    println (name)
+    print ("hobi saya ")
+    println (hobby)
+}
